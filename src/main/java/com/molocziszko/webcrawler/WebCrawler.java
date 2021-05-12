6755a97e1b9d1e61daa61705f0f9b75c14dc3af8
molocziszko <vovvich91@gmail.com>
2021-05-13 01:37:48 +0300

    feat: implement getting page via HTTPClient and Jsoup

--- src/main/java/com/molocziszko/webcrawler/WebCrawler.java
+++ src/main/java/com/molocziszko/webcrawler/WebCrawler.java
@@ -1,4 +1,53 @@
 package com.molocziszko.webcrawler;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class WebCrawler {
+    private static final int MAX_DEPTH = 8;
+    private static final int MAX_VISITED_PAGES = 10_000;
+
+    private final Deque<CrawlURL> listOfPagesToCrawl;
+    private final Deque<String> listOfVisitedPages;
+    private final String keywordList;
+
+    public WebCrawler(CrawlURL seedURL, String termsToSearch) {
+        this.listOfPagesToCrawl = new ArrayDeque<>(10_000);
+        this.listOfVisitedPages = new ArrayDeque<>(10_000);
+        listOfPagesToCrawl.add(seedURL);
+        keywordList = termsToSearch;
+    }
+
+    private Document getPage(int depth, String url, Deque<String> listOfPages) {
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            var statusCode = response.statusCode();
+            var textToParse = response.body();
+
+            Document doc = Jsoup.parse(textToParse);
+
+            if (statusCode == 200) {
+                if (!listOfPages.contains(url)) {
+                    listOfPages.add(url);
+                }
+                return doc;
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
